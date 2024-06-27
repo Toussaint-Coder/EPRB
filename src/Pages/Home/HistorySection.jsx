4d6ab9e8b44dd79bde590ca9f0a8a642f03110bd
2.saint <121985376+Toussaint-Coder@users.noreply.github.com>
2024-06-27 07:24:50 +0200

    added a new features on the But section

--- src/Pages/Home/HistorySection.jsx
+++ src/Pages/Home/HistorySection.jsx
@@ -1,6 +1,35 @@
 import Container from "../../Components/Container"
 import Button from "../../Components/Button"
+import React from "react"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 export default function HistorySection() {
+  const Buts = [
+    {
+      but: `L'Église Protestante du Burundi (EPRB) place au cœur de sa mission la promotion et le renforcement de la foi chrétienne parmi ses membres et dans la société burundaise. Son but premier est de diffuser l'Évangile de Jésus-Christ à travers des enseignements bibliques profonds et accessibles, des cultes réguliers imprégnés de louange et de prière, et des programmes de formation spirituelle. En formant des disciples engagés, l'EPRB aspire à voir ses membres vivre leur foi de manière tangible, influençant positivement leurs familles, leurs communautés et au-delà.`,
+    },
+    {
+      but: `L'EPRB s'engage fermement à contribuer au développement social et économique des communautés burundaises. Son objectif est de répondre aux besoins urgents et fondamentaux des populations les plus vulnérables. Cela se concrétise par la création et la gestion de centres de santé communautaires, la mise en place de programmes de nutrition pour lutter contre la malnutrition, et la distribution de nourriture et de vêtements aux familles dans le besoin. En outre, l'Église joue un rôle crucial dans la réponse aux crises humanitaires en offrant un soutien vital aux déplacés internes et aux réfugiés, tout en plaidant activement pour la justice sociale et l'équité au sein de la société.`,
+    },
+    {
+      but: `L'EPRB considère l'éducation comme un levier puissant pour le développement global et individuel. Son objectif est de fournir une éducation de qualité qui intègre des principes chrétiens dans le curriculum, aidant ainsi les jeunes à grandir dans la foi tout en acquérant les compétences nécessaires pour réussir dans la vie. L'Église établit et gère des écoles primaires et secondaires, offrant un environnement d'apprentissage sûr et enrichissant. De plus, elle soutient des centres de formation professionnelle où les jeunes et les adultes peuvent acquérir des compétences pratiques telles que la menuiserie, la couture et l'informatique. L'EPRB investit également dans des instituts théologiques pour former des leaders spirituels et des pasteurs qui guideront les futures générations. Par le biais de programmes de bourses et d'aides financières, l'Église rend l'éducation accessible aux enfants défavorisés, leur offrant ainsi une chance égale de réaliser leur potentiel et de contribuer positivement à la société.`,
+    },
+  ]
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    className:
+      "max-w-4xl w-full pl-4 border-l-2 border-l-white py-4 flex items-center ",
+    pauseOnHover: true,
+    arrows: false,
+    rtl: true,
+  }
+
   return (
     <>
       <div className="w-full min-h-80 flex flex-col gap-10">
@@ -12,17 +41,16 @@ export default function HistorySection() {
             <Button className="text-sm">En Savoir Plus</Button>
           </div>
         </Container>
-        <Container className="heroSection min-h-96">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
-            asperiores aspernatur, sapiente laudantium nemo hic quas porro
-            aliquid mollitia voluptatem temporibus et, incidunt qui est fuga
-            dolorum fugit rem illum dolore voluptas. A, quo repellat rem
-            nesciunt porro saepe sit suscipit vitae in quaerat eum cupiditate
-            beatae officia velit corrupti eligendi esse fuga sunt quis, facilis
-            numquam recusandae corporis dolorem magnam? Distinctio saepe
-            molestiae rerum sit deleniti excepturi.
-          </p>
+        <Container className="butSection min-h-96 flex items-center justify-center">
+          <div className="">
+            <Slider {...settings}>
+              {Buts.map((But) => (
+                <div>
+                  <p className="text-white italic leading-7">{But.but}</p>
+                </div>
+              ))}
+            </Slider>
+          </div>
         </Container>
       </div>
     </>
--- src/Pages/Home/MissionSection.jsx
+++ src/Pages/Home/MissionSection.jsx
@@ -4,59 +4,56 @@ import StudentsIcon from "../../assets/students.svg"
 import ChartIcon from "../../assets/chart.svg"
 import CrossIcon from "../../assets/cros.svg"
 
-/**
- * Renders a section of a webpage that outlines the missions of an organization.
- * @returns {JSX.Element} JSX element that renders the mission section of a webpage with a title, button, and a list of missions.
- */
 export default function MissionSection() {
   const Missions = [
     {
       key: 0,
       Icon: StudentsIcon,
-      Title: `Partage de l'Évangile et Expansion de la Foi Chrétienne`,
+      Tittle: `Partage de l'Évangile et Expansion de la Foi Chrétienne`,
       Description: `L'EPRB s'engage à partager l'Évangile de Jésus-Christ avec le plus grand nombre de personnes possible. Elle organise régulièrement des campagnes d'évangélisation dans les villes et villages, incluant des rassemblements publics et des séminaires. L'Église forme des missionnaires locaux et internationaux, leur fournissant les outils nécessaires pour prêcher efficacement, et collabore avec d'autres églises et organisations chrétiennes pour maximiser l'impact de ses efforts. En outre, l'EPRB utilise des médias modernes comme la radio, la télévision, et les plateformes en ligne pour diffuser des messages évangéliques à un public plus large.`,
     },
     {
       key: 1,
       Icon: ChartIcon,
-      Title: `Soutien aux Communautés et Promotion du Bien-être Social`,
+      Tittle: `Soutien aux Communautés et Promotion du Bien-être Social`,
       Description: `L'EPRB œuvre pour améliorer les conditions de vie des communautés locales par divers projets sociaux et humanitaires. Elle établit et soutient des centres de santé et des cliniques, organise des programmes de nutrition et distribue de la nourriture aux familles nécessiteuses. En réponse aux crises, l'Église fournit une aide humanitaire aux personnes déplacées et aux réfugiés. De plus, l'EPRB promeut la justice sociale en soutenant des initiatives de défense des droits de l'homme et en luttant contre les discriminations.`,
     },
     {
       key: 2,
       Icon: CrossIcon,
-      Title: `Développement de l'Éducation et Formation des Jeunes`,
+      Tittle: `Développement de l'Éducation et Formation des Jeunes`,
       Description: `L'EPRB considère l'éducation comme un levier essentiel pour le développement. Elle crée et gère des écoles primaires et secondaires, offrant une éducation de qualité intégrant des valeurs chrétiennes. L'Église établit des centres de formation professionnelle pour aider les jeunes à acquérir des compétences pratiques et soutient des instituts théologiques pour former des leaders religieux. Pour garantir l'accès à l'éducation, l'EPRB offre des bourses et des aides financières aux étudiants défavorisés, leur permettant de poursuivre leurs études sans contraintes économiques.`,
     },
   ]
-
   return (
-    <div className="w-full min-h-80 flex flex-col gap-10">
-      <Container className="flex justify-between items-center h-10 border-l-2 border-primary">
-        <div className="pl-4">
-          <h2 className="text-xl text-primary">Notre Mission</h2>
-        </div>
-        <div>
-          <Button className="text-sm">En Savoir Plus</Button>
-        </div>
-      </Container>
-      {Missions.map((Mission) => (
-        <Container className="flex flex-col" key={Mission.key}>
-          <div className="w-full grid grid-cols-2 gap-5">
-            <div className="w-10">
-              <img src={Mission.Icon} alt="Icon" className="" />
-            </div>
-            <div className="row-span-1">
-              <h2 className="text-primary text-xl font-semibold">
-                {Mission.Title}
-              </h2>
-              <p className="text-sm leading-6 text-black/85">
-                {Mission.Description}
-              </p>
-            </div>
+    <>
+      <div className="w-full min-h-80 flex flex-col gap-10">
+        <Container className="flex justify-between items-center h-10 border-l-2 border-primary">
+          <div className="pl-4">
+            <h2 className="text-xl text-primary">Notre Mission</h2>
+          </div>
+          <div>
+            <Button className="text-sm">En Savoir Plus</Button>
           </div>
         </Container>
-      ))}
-    </div>
+        {Missions.map((Mission) => (
+          <Container className="flex flex-col " key={Mission.key}>
+            <div className="w-full flex gap-5">
+              <div className="w-16">
+                <img src={Mission.Icon} alt="Icon" className="w-16" />
+              </div>
+              <div className="flex-1">
+                <h2 className="text-primary text-xl font-semibold">
+                  {Mission.Tittle}
+                </h2>
+                <p className="text-sm leading-6 text-black/85">
+                  {Mission.Description}
+                </p>
+              </div>
+            </div>
+          </Container>
+        ))}
+      </div>
+    </>
   )
 }
